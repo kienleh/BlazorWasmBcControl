0685e3f3c514cb7f664039c0a3a4e7f802c9de62
Jan Runge <runge@bss-it.de>
2020-09-08 09:59:50 +0200

    gitattribute file added https://github.com/dotnet/aspnetcore/issues/19796

--- make-dist.ps1
+++ make-dist.ps1
@@ -2,3 +2,5 @@ Remove-Item -Recurse .\docs\ -Force
 dotnet publish -o docs
 Move-Item  .\docs\wwwroot\* .\docs
 New-Item -Type File -Path ./docs/.nojekyll
+New-Item -Type File -Path ./docs/.gitattributes
+Add-Content -Path .\docs\.gitattributes -Value '* binary'
